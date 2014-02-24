b682ed6dcb180671c6a71b1f9946deb1eed4b7d4
Alpha_Male <sad>
2014-02-24 16:01:00 -0500

    Commented, made my mark - Terry Tong

--- 2ME3 Checkers/2ME3 Checkers/Game1.cs
+++ 2ME3 Checkers/2ME3 Checkers/Game1.cs
@@ -11,6 +11,7 @@ using Microsoft.Xna.Framework.Media;
 
 namespace _2ME3_Checkers
 {
+    /// hello this is Terry Tong Checking in Please delete this after
     /// <summary>
     /// Currently:
     ///   * The three states work by keyboard hotkeys = { M, P, S }
