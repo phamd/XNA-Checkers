41191ae0bafa4edd1a4b32003bfc0bb9f3c8c2e0
Don Pham <phamd@mcmaster.ca>
2014-03-19 23:21:05 -0400

    Complete: setValidMovements unit tests for the default setup and a midgame setup with kings.

--- 2ME3 CheckersTest/UnitTests.cs
+++ 2ME3 CheckersTest/UnitTests.cs
@@ -70,7 +70,9 @@ namespace _2ME3_CheckersTest
         [TestMethod]
         public void BoardLegalMovesTest_DefaultSetup()
         {
+            // The default new game board
             String boardSetup = "A1=W,A3=W,A7=B,B2=W,B6=B,B8=B,C1=W,C3=W,C7=B,D2=W,D6=B,D8=B,E1=W,E3=W,E7=B,F2=W,F6=B,F8=B,G1=W,G3=W,G7=B,H2=W,H6=B,H8=B";
+            // The legal moves of each piece (four possible diagonal directions). Null indicates an empty space on the board.
             String[][] expectedValidMoves = new String[64][] { new String[] { "illegal", "illegal", "illegal", "illegal" }, new String[] { null }, new String[] { "illegal", "1,3", "illegal", "illegal" }, new String[] { null }, new String[] { null }, new String[] { null }, new String[] { "illegal", "illegal", "illegal", "illegal" }, new String[] { null }, new String[] { null }, new String[] { "illegal", "illegal", "illegal", "illegal" }, new String[] { null }, new String[] { null }, new String[] { null }, new String[] { "illegal", "illegal", "2,4", "0,4" }, new String[] { null }, new String[] { "illegal", "illegal", "illegal", "illegal" }, new String[] { "illegal", "illegal", "illegal", "illegal" }, new String[] { null }, new String[] { "1,3", "3,3", "illegal", "illegal" }, new String[] { null }, new String[] { null }, new String[] { null }, new String[] { "illegal", "illegal", "illegal", "illegal" }, new String[] { null }, new String[] { null }, new String[] { "illegal", "illegal", "illegal", "illegal" }, new String[] { null }, new String[] { null }, new String[] { null }, new String[] { "illegal", "illegal", "4,4", "2,4" }, new String[] { null }, new String[] { "illegal", "illegal", "illegal", "illegal" }, new String[] { "illegal", "illegal", "illegal", "illegal" }, new String[] { null }, new String[] { "3,3", "5,3", "illegal", "illegal" }, new String[] { null }, new String[] { null }, new String[] { null }, new String[] { "illegal", "illegal", "illegal", "illegal" }, new String[] { null }, new String[] { null }, new String[] { "illegal", "illegal", "illegal", "illegal" }, new String[] { null }, new String[] { null }, new String[] { null }, new String[] { "illegal", "illegal", "6,4", "4,4" }, new String[] { null }, new String[] { "illegal", "illegal", "illegal", "illegal" }, new String[] { "illegal", "illegal", "illegal", "illegal" }, new String[] { null }, new String[] { "5,3", "7,3", "illegal", "illegal" }, new String[] { null }, new String[] { null }, new String[] { null }, new String[] { "illegal", "illegal", "illegal", "illegal" }, new String[] { null }, new String[] { null }, new String[] { "illegal", "illegal", "illegal", "illegal" }, new String[] { null }, new String[] { null }, new String[] { null }, new String[] { "illegal", "illegal", "illegal", "6,4" }, new String[] { null }, new String[] { "illegal", "illegal", "illegal", "illegal" } };
             BoardLegalMovesTest(boardSetup, expectedValidMoves);
         }
@@ -78,7 +80,10 @@ namespace _2ME3_CheckersTest
         [TestMethod]
         public void BoardLegalMovesTest_MidgameSetup()
         {
-            String boardSetup = "K9=BK";
+            // A setup of the middle of a checkers match with multiple kings.
+            String boardSetup = "A1=W,A3=W,A7=B,B4=B,B8=B,C1=BK,C5=B,C7=W,D4=BK,D6=W,E3=W,F4=B,F6=B,F8=WK,G1=W,G3=W,G5=W,G7=WK,H6=B";
+            String[][] expectedValidMoves = new String[64][] { new String[] { "illegal", "1,1", "illegal", "illegal" }, new String[] { null }, new String[] { "illegal", "illegal", "illegal", "illegal" }, new String[] { null }, new String[] { null }, new String[] { null }, new String[] { "illegal", "illegal", "1,5", "illegal" }, new String[] { null }, new String[] { null }, new String[] { null }, new String[] { null }, new String[] { "illegal", "illegal", "2,2", "0,1" }, new String[] { null }, new String[] { null }, new String[] { null }, new String[] { "illegal", "illegal", "illegal", "illegal" }, new String[] { "1,1", "3,1", "3,-99", "1,-99" }, new String[] { null }, new String[] { null }, new String[] { null }, new String[] { "illegal", "illegal", "illegal", "illegal" }, new String[] { null }, new String[] { "0,7", "3,7", "illegal", "illegal" }, new String[] { null }, new String[] { null }, new String[] { null }, new String[] { null }, new String[] { "illegal", "4,4", "5,1", "2,2" }, new String[] { null }, new String[] { "illegal", "4,6", "illegal", "illegal" }, new String[] { null }, new String[] { null }, new String[] { null }, new String[] { null }, new String[] { "illegal", "illegal", "illegal", "illegal" }, new String[] { null }, new String[] { null }, new String[] { null }, new String[] { null }, new String[] { null }, new String[] { null }, new String[] { null }, new String[] { null }, new String[] { "illegal", "illegal", "7,1", "3,1" }, new String[] { null }, new String[] { "illegal", "illegal", "7,3", "4,4" }, new String[] { null }, new String[] { "4,-99", "6,-99", "illegal", "4,6" }, new String[] { "5,1", "7,1", "illegal", "illegal" }, new String[] { null }, new String[] { "4,4", "7,3", "illegal", "illegal" }, new String[] { null }, new String[] { "4,6", "7,6", "illegal", "illegal" }, new String[] { null }, new String[] { "illegal", "7,7", "7,4", "4,4" }, new String[] { null }, new String[] { null }, new String[] { null }, new String[] { null }, new String[] { null }, new String[] { null }, new String[] { "illegal", "illegal", "illegal", "illegal" }, new String[] { null }, new String[] { null } };
+            BoardLegalMovesTest(boardSetup, expectedValidMoves);
         }
 
         public void BoardLegalMovesTest(String boardSetup, String[][] expectedValidMoves)
@@ -120,52 +125,5 @@ namespace _2ME3_CheckersTest
                 }
             }
         }
-
-
-
-        /* Board setup valid movements to array generator
-        [TestMethod]
-        public void GenerateActualResult()
-        {
-            using (Game1 game = new Game1())
-            {
-                String output;
-                Board board = new Board();
-                String[] actualResult = new String[4];
-                board.setUpBoard("A1=W,A3=W,A7=B,B2=W,B6=B,B8=B,C1=W,C3=W,C7=B,D2=W,D6=B,D8=B,E1=W,E3=W,E7=B,F2=W,F6=B,F8=B,G1=W,G3=W,G7=B,H2=W,H6=B,H8=B");
-                game.setValidMovements(board);
-
-                output = "expectedOutput = new String[64] { ";
-                for (int col = 0; col < 8; col++)
-                {
-                    for (int row = 0; row < 8; row++)
-                    {
-                        try
-                        {
-                            Piece piece = board.getPiece(col, row);
-                            output += "new String[]{";
-                            for (int i = 0; i < 4; i++)
-                            {
-                                actualResult[i] = board.getPiece(col, row).getValidMovements()[i].ToString();
-                                if (actualResult[i].Split(',')[0] == "-99") actualResult[i] = "illegal";
-                                output += '"' + actualResult[i] + '"';
-                                if (i != 3) output += ",";
-                            }
-                            output += "}, ";
-                        }
-                        catch
-                        {
-                            output += "null}, ";
-                        }
-                    }
-                }
-                Console.WriteLine(output);
-            }
-        }
-        */
-
-
-
-
     }
 }
